1dfa61a278b8a3e88b8839d7dd6d7ad9d8f90049
André Matos Erdei <53622777+andrerdei@users.noreply.github.com>
2020-02-20 03:37:13 -0300

    Configurando Template Engine Para o index.html

--- public/js/server.js
+++ public/js/server.js
@@ -4,23 +4,47 @@ const express = require("express");
 const server = express();
 
 
-// Configurando Template Engine
-
-const nunjucks = require("nunjucks");
-nunjucks.configure("./", {
-    express: server
-});
-
-
 // Configurando Arquivos Extras Para Template engine
 
 server.use(express.static("public"));
 
 
+// Configurando Template Engine
+
+const nunjucks = require("nunjucks");
+
+nunjucks.configure("./", {
+    express: server,
+    noCache: true
+});
+
+
+// Lista de Doadores
+
+const doadores = [
+    {
+        nome: "Diego Fernandes",
+        sangue: "AB +"
+    },
+    {
+        nome: "Cleiton souza",
+        sangue: "B +"
+    },
+    {
+        nome: "Robson Marques",
+        sangue: "A -"
+    },
+    {
+        nome: "André Erdei",
+        sangue: "O +"
+    }
+];
+
+
 // Configurando Apresentação da Página
 
 server.get("/", (req, res) => {
-    return res.render("index.html");
+    return res.render("index.html", {doadores});
 });
 
 
